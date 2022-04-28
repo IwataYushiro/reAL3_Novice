5925190de3ccb4dc9c83d59b5a9b45bed6ac12f5
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-04-28 14:00:44 +0900

    新KAMATAENGINEにアップグレード

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
-﻿#include <Novice.h>
+﻿#include "Math.h"
+#include <Novice.h>
+#define PI 3.14159265
 
-const char kWindowTitle[] = "学籍番号";
+const char kWindowTitle[] = "LE2B_07_イワタ_ユウシロウ";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -8,6 +10,156 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
+	//ベースとなる行列の宣言と初期化
+	float boxBase[4][3] = {
+	  {100.0f, 100.0f, 1.0f},
+	  {100.0f, 400.0f, 1.0f},
+	  {400.0f, 400.0f, 1.0f},
+	  {400.0f, 100.0f, 1.0f}
+    };
+
+	//原点へ
+	float boxOrigin[4][3] = {
+	  {100.0f, 100.0f, 1.0f},
+	  {100.0f, 400.0f, 1.0f},
+	  {400.0f, 400.0f, 1.0f},
+	  {400.0f, 100.0f, 1.0f}
+    };
+	// 移動・回転・拡大の行列の宣言と初期化
+	float boxMoved[4][3];
+	float boxRotated[4][3];
+	float boxScaled[4][3];
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 3; j++) {
+			boxMoved[i][j] = boxBase[i][j];
+			boxRotated[i][j] = boxBase[i][j];
+			boxScaled[i][j] = boxBase[i][j];
+		}
+	}
+
+	// 平行移動のためのアフィン行列
+
+	float affinMove[3][3] = {
+	  {1.0f, 0.0f, 100.0f},
+      {0.0f, 1.0f, 100.0f},
+      {0.0f, 0.0f, 1.0f  }
+    };
+
+	// 回転のためのアフィン行列
+
+	//原点へ平行移動する用
+	float affinMoveOrigin[3][3] = {
+	  {1.0f, 0.0f, -100.0f},
+      {0.0f, 1.0f, -100.0f},
+      {0.0f, 0.0f, 1.0f   }
+    };
+
+	//回転処理用
+	float affinRotated[3][3] = {
+	  {cos(PI / 4), -sin(PI / 4), 0.0f},
+      {sin(PI / 4), cos(PI / 4),  0.0f},
+      {0.0f,        0.0f,         1.0f}
+    };
+
+	//元居た位置に戻る用
+	float affinMoveAfter[3][3] = {
+	  {1.0f, 0.0f, 100.0f},
+      {0.0f, 1.0f, 100.0f},
+      {0.0f, 0.0f, 1.0f  }
+    };
+
+	// 拡大のためのアフィン行列
+	float affinScaled[3][3]{
+	  {2.0f, 0.0f, 0.0f},
+      {0.0f, 2.0f, 0.0f},
+      {0.0f, 0.0f, 1.0f}
+    };
+
+	// 平行移動の計算
+	for (int i = 0; i < 4; i++) {
+
+		boxMoved[i][0] = affinMove[0][0] * boxBase[i][0] + affinMove[0][1] * boxBase[i][1] +
+		                 affinMove[0][2] * boxBase[i][2];
+
+		boxMoved[i][1] = affinMove[1][0] * boxBase[i][0] + affinMove[1][1] * boxBase[i][1] +
+		                 affinMove[1][2] * boxBase[i][2];
+
+		boxMoved[i][2] = affinMove[2][0] * boxBase[i][0] + affinMove[2][1] * boxBase[i][1] +
+		                 affinMove[2][2] * boxBase[i][2];
+	}
+
+	// 回転の計算
+
+	//原点へ移動する
+	for (int i = 0; i < 4; i++) {
+
+		boxOrigin[i][0] = affinMoveOrigin[0][0] * boxBase[i][0] +
+		                  affinMoveOrigin[0][1] * boxBase[i][1] +
+		                  affinMoveOrigin[0][2] * boxBase[i][2];
+
+		boxOrigin[i][1] = affinMoveOrigin[1][0] * boxBase[i][0] +
+		                  affinMoveOrigin[1][1] * boxBase[i][1] +
+		                  affinMoveOrigin[1][2] * boxBase[i][2];
+
+		boxOrigin[i][2] = affinMoveOrigin[2][0] * boxBase[i][0] +
+		                  affinMoveOrigin[2][1] * boxBase[i][1] +
+		                  affinMoveOrigin[2][2] * boxBase[i][2];
+	}
+	//回転処理
+	for (int i = 0; i < 4; i++) {
+		boxRotated[i][0] = affinRotated[0][0] * boxOrigin[i][0] +
+		                   affinRotated[0][1] * boxOrigin[i][1] +
+		                   affinRotated[0][2] * boxOrigin[i][2];
+
+		boxRotated[i][1] = affinRotated[1][0] * boxOrigin[i][0] +
+		                   affinRotated[1][1] * boxOrigin[i][1] +
+		                   affinRotated[1][2] * boxOrigin[i][2];
+
+		boxRotated[i][2] = affinRotated[2][0] * boxOrigin[i][0] +
+		                   affinRotated[2][1] * boxOrigin[i][1] +
+		                   affinRotated[2][2] * boxOrigin[i][2];
+	}
+	//元居た位置に戻す
+	for (int i = 0; i < 4; i++) {
+		boxRotated[i][0] = affinMoveAfter[0][0] * boxRotated[i][0] +
+		                   affinMoveAfter[0][1] * boxRotated[i][1] +
+		                   affinMoveAfter[0][2] * boxRotated[i][2];
+
+		boxRotated[i][1] = affinMoveAfter[1][0] * boxRotated[i][0] +
+		                   affinMoveAfter[1][1] * boxRotated[i][1] +
+		                   affinMoveAfter[1][2] * boxRotated[i][2];
+
+		boxRotated[i][2] = affinMoveAfter[2][0] * boxRotated[i][0] +
+		                   affinMoveAfter[2][1] * boxRotated[i][1] +
+		                   affinMoveAfter[2][2] * boxRotated[i][2];
+	}
+	// 拡大の計算
+	//
+	for (int i = 0; i < 4; i++) {
+
+		boxScaled[i][0] = affinScaled[0][0] * boxOrigin[i][0] +
+		                  affinScaled[0][1] * boxOrigin[i][1] + affinScaled[0][2] * boxOrigin[i][2];
+
+		boxScaled[i][1] = affinScaled[1][0] * boxOrigin[i][0] +
+		                  affinScaled[1][1] * boxOrigin[i][1] + affinScaled[1][2] * boxOrigin[i][2];
+
+		boxScaled[i][2] = affinScaled[2][0] * boxOrigin[i][0] +
+		                  affinScaled[2][1] * boxOrigin[i][1] + affinScaled[2][2] * boxOrigin[i][2];
+	}
+	for (int i = 0; i < 4; i++) {
+		boxScaled[i][0] = affinMoveAfter[0][0] * boxScaled[i][0] +
+		                  affinMoveAfter[0][1] * boxScaled[i][1] +
+		                  affinMoveAfter[0][2] * boxScaled[i][2];
+
+		boxScaled[i][1] = affinMoveAfter[1][0] * boxScaled[i][0] +
+		                  affinMoveAfter[1][1] * boxScaled[i][1] +
+		                  affinMoveAfter[1][2] * boxScaled[i][2];
+
+		boxScaled[i][2] = affinMoveAfter[2][0] * boxScaled[i][0] +
+		                  affinMoveAfter[2][1] * boxScaled[i][1] +
+		                  affinMoveAfter[2][2] * boxScaled[i][2];
+	}
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
@@ -33,7 +185,54 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
+		/// 0,ベース
+		for (int i = 0; i < 4; i++) {
+			Novice::DrawLine(
+			  boxBase[i][0], boxBase[i][1], boxBase[(i + 1) % 4][0], boxBase[(i + 1) % 4][1],
+			  BLACK);
+		}
+		/// 1,平行移動
+
+		for (int i = 0; i < 4; i++) {
+			Novice::DrawLine(
+			  boxMoved[i][0], boxMoved[i][1], boxMoved[(i + 1) % 4][0], boxMoved[(i + 1) % 4][1],
+			  BLUE);
+		}
+		// 2,回転
+		for (int i = 0; i < 4; i++) {
+			Novice::DrawLine(
+			  boxRotated[i][0], boxRotated[i][1], boxRotated[(i + 1) % 4][0],
+			  boxRotated[(i + 1) % 4][1], RED);
+		}
+
+		// 3,拡大
+		for (int i = 0; i < 4; i++) {
+			Novice::DrawLine(
+			  boxScaled[i][0], boxScaled[i][1], boxScaled[(i + 1) % 4][0],
+			  boxScaled[(i + 1) % 4][1], GREEN);
+		}
+		//
+		/*for (int i = 0; i < 4; i++) {
+		    Novice::DrawLine(
+		      box[i][0] * 1/2 + box[i][1] * 0 + box[i][2] * 0,
+		      box[i][0] * 0 + box[i][1] * 1/2 + box[i][2] * 0,
+		      box[(i + 1) % 4][0] * 1/2 + box[(i + 1) % 4][1] * 0 + box[i][2] * 0,
+		      box[(i + 1) % 4][0] * 0 + box[(i + 1) % 4][1] * 1/2 + box[i][2] * 0, WHITE);
+		}*/
+
 		///
+		/*/// 0,全部(拡大)
+		for (int i = 0; i < 4; i++) {
+		    Novice::DrawLine(
+		      box[i][0] * cos(PI / 4) * 2 + box[i][1] * -sin(PI / 4) * -2 + box[i][2] * 1,
+		      box[i][0] * sin(PI / 4) * -2 + box[i][1] * cos(PI / 4) * 2 + box[i][2] * 1,
+		      box[(i + 1) % 4][0] * cos(PI / 4) * 2 + box[(i + 1) % 4][1] * -sin(PI / 4) * -2 +
+		        box[i][2] * 1,
+		      box[(i + 1) % 4][0] * sin(PI / 4) * -2 + box[(i + 1) % 4][1] * cos(PI / 4) * 2 +
+		        box[i][2] * 1,
+		      BLACK);
+		}*/
+
 		/// ↑描画処理ここまで
 		///
 
